0babacb0fc86f7561d1196899b07bce65be69b7d
Bodacious <gavin@gavinmorrice.com>
2018-02-02 17:15:33 +0000

    Update view helpers for Rails 5.x

--- lib/wisepdf/helper.rb
+++ lib/wisepdf/helper.rb
@@ -35,16 +35,16 @@ module Wisepdf
         sources.collect { |source|
           filename = ( source =~ /.css\z/ ? source : source + '.css' )
           "<style type='text/css'>
-            #{::Rails.application.assets.find_asset(filename)}
+            #{build_env.find_asset(filename)}
           </style>"
         }.join("\n").html_safe
       end
 
       def wisepdf_image_tag(img, options={})
-        if File.exists? img
+        if File.exists?(img)
           image_tag "file://#{img}", options
-        elsif asset = ::Rails.application.assets.find_asset(img)
-          image_tag "file:///#{asset.pathname.to_s}", options
+        elsif asset = build_env.find_asset(img)
+          image_tag "file:///#{asset.pathname}", options
         end
       end
 
@@ -53,10 +53,16 @@ module Wisepdf
           filename = ( source =~ /.js\z/ ? source : source + '.js' )
           "<script type='text/javascript'>
             //<![CDATA[
-              #{::Rails.application.assets.find_asset(filename)}
+              #{build_env.find_asset(filename)}
             //]]>
           </script>"
         }.join("\n").html_safe
+      end
+
+      private
+
+      def build_env
+        ::Sprockets::Railtie.build_environment(::Rails.application)
       end
     end
   end
